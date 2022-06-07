02854d621aa5b3797a2cd098f55b6405c3a55f4b
wahliyudin <wahliyudinzein@gmail.com>
2022-06-07 08:41:40 +0700

    RoleSeeder dan UserSeeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'name' => 'admin',
+            'display_name' => 'User Administrator', // optional
+            'description' => 'User is allowed to manage and edit other users', // optional
+        ]);
+
+        Role::create([
+            'name' => 'siswa',
+            'display_name' => 'Siswa', // optional
+            'description' => 'Siswa', // optional
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456789'), // password
+            'remember_token' => Str::random(10),
+        ])->attachRole('admin');
+    }
+}
