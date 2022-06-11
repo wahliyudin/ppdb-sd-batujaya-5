683a40ed477924340e236a1876fbdc762b7e1af9
wahliyudin <wahliyudinzein@gmail.com>
2022-06-11 09:53:05 +0700

    Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -15,7 +17,9 @@ class DashboardController extends Controller
                 'path' => [
                     'Dashboard' => 0
                 ]
-            ]
+            ],
+            'jumlah_siswa' => User::whereRoleIs('siswa')->count(),
+            'total_pendapatan' => Payment::sum('total_bayar')
         ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,35 +3,37 @@
 @section('content')
     <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
-        <div class="row">
+        <div class="row justify-content-center">
             <div class="col-lg-4 col-6">
                 <!-- small box -->
                 <div class="small-box bg-secondary">
                     <div class="inner">
-                        <h3>100</h3>
+                        <h3>{{ $jumlah_siswa }}</h3>
 
                         <p>Jumlah Siswa</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-users"></i>
                     </div>
-                    <a href="" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('admin.students.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
-            {{-- <div class="col-lg-4 col-6">
+            <div class="col-lg-4 col-6">
                 <!-- small box -->
                 <div class="small-box bg-primary">
                     <div class="inner">
-                        <h3>100</h3>
+                        <h3>{{ numberFormat($total_pendapatan, 'Rp.') }}</h3>
 
-                        <p>Jumlah Kela</p>
+                        <p>Total Pendapatan</p>
                     </div>
                     <div class="icon">
-                        <i class="fas fa-tags"></i>
+                        <i class="fas fa-money-bill-wave"></i>
                     </div>
-                    <a href="" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('admin.payments.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
-            </div> --}}
+            </div>
         </div>
     </div><!-- /.container-fluid -->
 @endsection
